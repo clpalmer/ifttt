2e9a5fac002098957559d498fa60e7307d8be1ef
Clancy Palmer <clancyp@gmail.com>
2018-04-18 14:02:31 -0700

    .

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+const dbConfig = require('./config/database.config.js')
+
+const app = express();
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// Configuring the DB
+mongoose.promise = global.Promise;
+mongoose.connect(dbConfig.url)
+  .then(() => {
+    console.log('DB Connected');
+  }).catch(err => {
+    console.log('Failed to connect to DB (' + err + ')');
+    process.exit();
+  });
+
+// define a simple route
+app.get('/', (req, res) => {
+  res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
+// listen for requests
+app.listen(3001, () => {
+  console.log("Server is listening on port 3000");
+});
