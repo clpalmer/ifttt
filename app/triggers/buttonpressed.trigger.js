b7b1681986cf8c9968909002e0eaf01b1c141b35
clancy.palmer <clancy.palmer@hp.com>
2018-04-18 15:57:15 -0700

    .

--- app/triggers/buttonpressed.trigger.js
+++ app/triggers/buttonpressed.trigger.js
@@ -17,7 +17,7 @@ const buttonPressedTrigger = (req, res) => {
               buttonId: bp.buttonId,
               meta: {
                 id: bp._id,
-                timestamp: bp.createdAt.getTime()
+                timestamp: bp.createdAt.getTime() / 1000
               }
             }
           })
