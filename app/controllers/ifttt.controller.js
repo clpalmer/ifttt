d4bcb6657f655a55c9585aea16b2ee4174ede50c
clancy.palmer <clancy.palmer@hp.com>
2018-04-18 15:51:21 -0700

    .

--- app/controllers/ifttt.controller.js
+++ app/controllers/ifttt.controller.js
@@ -1,7 +1,7 @@
 const IFTTTConfig = require('../../config/ifttt.config.js');
 
 module.exports.status = (req, res) => {
-  if (req.headers['HTTP_IFTTT_SERVICE_KEY'] !== IFTTTConfig.serviceKey) {
+  if (req.headers['IFTTT-Service-Key'] !== IFTTTConfig.serviceKey) {
     return res.status(401).send({
       message: 'Unauthorized'
     });
@@ -11,7 +11,7 @@ module.exports.status = (req, res) => {
 };
 
 module.exports.setup = (req, res) => {
-  if (req.headers['HTTP_IFTTT_SERVICE_KEY'] !== IFTTTConfig.serviceKey) {
+  if (req.headers['IFTTT-Service-Key'] !== IFTTTConfig.serviceKey) {
     return res.status(401).send({
       message: 'Unauthorized'
     });
