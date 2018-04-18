ba43b148d5dbdabdb633c6c24b4d122a1da2f3e1
clancy.palmer <clancy.palmer@hp.com>
2018-04-18 15:56:00 -0700

    .

--- config/ifttt.config.js
+++ config/ifttt.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  serviceKey: "YTzrZMOUbIJ0_4tP7f4M_gWPDbJA6aaOkoRCq-p-f4pdv6atz5WfhIMTzxDuW7ZoP"
+  serviceKey: "YTzrZMOUbIJ0_4tP7f4M_gWPDbJA6aaOkoRCq-p-f4pdv6atz5WfhIMTzxDuW7Zo"
 }
