b87d9d83072d459b1887e58fe4412c36a7afa3d8
Clancy Palmer <clancyp@gmail.com>
2018-05-02 14:27:38 -0700

    Updates

--- scripts/seed.db.js
+++ scripts/seed.db.js
@@ -8,55 +8,58 @@ const User = require('../app/models/user.model.js');
 const bcrypt = require('bcrypt');
 
 mongoose.promise = global.Promise;
-mongoose.connect(dbConfig.url)
-  .then(() => {
-    console.log('DB Connected');
-    OAuthAuthorizationCode.find().remove();
-    OAuthAccessToken.find().remove();
-    OAuthRefreshToken.find().remove();
-    OAuthClient.find().remove();
-    User.find().remove();
-
-    User.create({
-      firstName: 'Clancy',
-      lastName: 'Palmer',
-      email: 'clancyp@gmail.com',
-      scope: 'ifttt',
-      password: bcrypt.hashSync('Password1', 10),
-    }).then((user) => {
-      OAuthClient.create({
-        name:  'Test Client',
-        id:  'test',
-        secret: 'testysecret1!',
-        redirectUris: 'http://localhost/cb',
-        grants: ['authorization_code', 'password', 'refresh_token', 'client_credentials'],
-        scope: 'test',
-        accessTokenLifetime: 86400,
-        refreshTokenLifetime: 3153600000,
-        user: user._id,
-      }).then(() => {});
+mongoose.connect(dbConfig.url).then(() => {
+  console.log('DB Connected');
+  OAuthAuthorizationCode.remove().then(() => {
+    OAuthAccessToken.remove().then(() => {
+      OAuthClient.remove().then(() => {
+        OAuthRefreshToken.remove().then(() => {
+          User.remove().then(() => {
+            User.create({
+              firstName: 'Clancy',
+              lastName: 'Palmer',
+              email: 'clancyp@gmail.com',
+              scope: 'ifttt',
+              password: bcrypt.hashSync('Password1', 10),
+            }).then((user) => {
+              OAuthClient.create({
+                name:  'Client',
+                id:  'client',
+                secret: 'testysecret1!',
+                redirectUris: 'http://localhost/cb',
+                grants: ['authorization_code', 'password', 'refresh_token', 'client_credentials'],
+                scope: 'ifttt',
+                accessTokenLifetime: 86400,
+                refreshTokenLifetime: 3153600000,
+                user: user._id,
+              }).then(() => {
+                User.create({
+                  firstName: 'IFTTT',
+                  lastName: 'User',
+                  email: 'ifttt@nowhere.com',
+                  scope: 'ifttt',
+                  password: bcrypt.hashSync('iftttpw1!', 10),
+                }).then((user) => {
+                  OAuthClient.create({
+                    name:  'IFTTT Client',
+                    id:  'ifttt',
+                    secret: 'iftttpw1!',
+                    redirectUris: 'https://ifttt.com/channels/nodeifttt/authorize',
+                    grants: ['authorization_code', 'refresh_token'],
+                    scope: 'ifttt',
+                    accessTokenLifetime: 86400,
+                    refreshTokenLifetime: 3153600000,
+                    user: user._id,
+                  }).then(() => {process.exit()});
+                });
+              });
+            });
+          });
+        });
+      });
     });
-
-    User.create({
-      firstName: 'IFTTT',
-      lastName: 'User',
-      email: 'ifttt@nowhere.com',
-      scope: 'ifttt',
-      password: bcrypt.hashSync('iftttpw1!', 10),
-    }).then((user) => {
-      OAuthClient.create({
-        name:  'IFTTT Client',
-        id:  'ifttt',
-        secret: 'iftttpw1!',
-        redirectUris: 'https://ifttt.com/channels/nodeifttt/authorize',
-        grants: ['authorization_code', 'refresh_token'],
-        scope: 'ifttt',
-        accessTokenLifetime: 86400,
-        refreshTokenLifetime: 3153600000,
-        user: user._id,
-      }).then(() => {});
-    });
-  }).catch(err => {
-    console.log('Failed to connect to DB (' + err + ')');
-    process.exit();
   });
+}).catch(err => {
+  console.log('Failed to connect to DB (' + err + ')');
+  process.exit();
+});
