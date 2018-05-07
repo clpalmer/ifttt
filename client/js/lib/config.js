7894fee432939fa314245f38b71f0249a339c6ef
clancy.palmer <clancy.palmer@hp.com>
2018-05-07 09:30:22 -0700

    .

--- client/js/lib/config.js
+++ client/js/lib/config.js
@@ -1,11 +1,7 @@
-import CustomConfig from './config.custom';
-
-const config = {
+export default {
   ifttt: {
-    host: 'https://<replace this or add ifttt object to config.custom.js>',
-    socketHost: 'wss://<replace this or add ifttt object to config.custom.js>',
+    host: 'https://<replace this>',
+    socketHost: 'wss://<replace this>',
     clientId: 'client',
   },
 };
-
-export default Object.assign(config, CustomConfig);
